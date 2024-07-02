a006e5b9aef998f89553438318207ddb5c8ac127
thirteentwenty <78246098+thirteentwenty@users.noreply.github.com>
2024-07-01 20:17:46 -1000

    Update Get-WebFile.ps1
    Added invoke-webrequest
    Added clear-host
    Added formatting changes because I'm vain

--- miscCrap/Get-WebFile.ps1
+++ miscCrap/Get-WebFile.ps1
@@ -1,27 +1,42 @@
 param (
     [string]$Filename
 )
-
-$server = "https://dlc.cetra.us/"
+Clear-Host
+$server = "https://some.domain.tld/"
+$saveLocation = "C:\Users\Someone\Some\Folder\"
 $webFile = $server + $($Filename)
+$date = (Get-Date -f 'yyyy.MM.dd-HHmm')
+$file = $Filename.split(".")
+$name = $file[0]
+$ext = $file[1]
+$fname = $file[0] + "_" + $date + "." + $file[1]
+
 
 function Get-FileSize {
     $fileSizeInBytes = [math]::Round((Invoke-WebRequest $($webFile) -Method Head).Headers.'Content-Length',2)
 
     If (($fileSizeInBytes -ge 1TB)) {
-        return "{0:N2} TB" -f ($fileSizeInBytes / 1TB)
+        return "Download size {0:N2} TB" -f ($fileSizeInBytes / 1TB)
     } elseif ($fileSizeInBytes -ge 1GB) {
-        return "{0:N2} GB" -f ($fileSizeInBytes / 1GB)
+        return "Download size {0:N2} GB" -f ($fileSizeInBytes / 1GB)
     } elseif ($fileSizeInBytes -ge 1MB) {
-        return "{0:N2} MB" -f ($fileSizeInBytes / 1MB)
+        return "Download size {0:N2} MB" -f ($fileSizeInBytes / 1MB)
     } else {
         Write-Host $($fileSizeInBytes)
     }
 }
 
 try {
+    Write-Host ""
+    Write-Host ""
+    Write-Host ""
+    Write-Host ""
+    Write-Host ""
+    Write-Host ""
+    Write-Host ""
     Get-FileSize -ErrorAction SilentlyContinue
-    Write-Host $Filename
+    Write-Host Saving $Filename to $saveLocation
+    Invoke-WebRequest -Uri $($webFile) -Outfile $saveLocation$fname
 } catch {
     if ($_.Exception.Response.StatusCode.Value__ -eq "404") {
         Write-Host File not found check the file name
